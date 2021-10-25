1e115230b60660954e6a41f77cc129f478b0ca4b
PC-DUSS <pierrecharles.dussault@gmail.com>
2021-10-25 02:26:13 -0400

    Starting TRAP service routines.

--- lc3.c
+++ lc3.c
@@ -7,10 +7,10 @@
  * */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdint.h>
 
 /*
-#include <string.h>
 #include <signal.h>
 
 #include <unistd.h>
@@ -40,6 +40,12 @@ void handle_STI(uint16_t instruction, uint16_t regs[]);
 void handle_STR(uint16_t instruction, uint16_t regs[]);
 void handle_TRAP(uint16_t instruction, uint16_t regs[]);
 void handle_BAD_OPCODE(uint16_t instruction, uint16_t regs[]);
+void handle_TRAP_GETC();
+void handle_TRAP_OUT();
+void handle_TRAP_PUTS();
+void handle_TRAP_IN();
+void handle_TRAP_PUTSP();
+void handle_TRAP_HALT();
 
 /* ----- VM Setup ----- */
 
@@ -94,9 +100,19 @@ enum {
 
 /* Condition flags */
 enum {
-  FL_P = 1 << 0, /* P Positive */
-  FL_Z = 1 << 1, /* Z Zero */
-  FL_N = 1 << 2 /* N Negative */
+  FL_P = 1 << 0, /* P Positive [001] */
+  FL_Z = 1 << 1, /* Z Zero [010] */
+  FL_N = 1 << 2 /* N Negative [100] */
+};
+
+/* Codes for TRAP routines */
+enum {
+  TRAP_GETC = 0x20, /* Get a char from keyboard, not echoed on the terminal */
+  TRAP_OUT = 0x21, /* Output a char */
+  TRAP_PUTS = 0x22, /* Output a word string */
+  TRAP_IN = 0x23, /* Get a char from keyboard, echoed on the terminal */
+  TRAP_PUTSP = 0x24, /* Output a byte string */
+  TRAP_HALT = 0x25 /* Halt the program */
 };
 /* end of VM Setup */
 
@@ -168,7 +184,27 @@ int main(int argc, const char* argv[]) {
         handle_STR(instruction, regs);
         break;
       case OP_TRAP:
-        handle_TRAP(instruction, regs);
+        switch(instruction & 0xFF) {
+          case TRAP_GETC:
+            handle_TRAP_GETC();
+            break;
+          case TRAP_OUT:
+            handle_TRAP_OUT();
+            break;
+          case TRAP_PUTS:
+            handle_TRAP_PUTS();
+            break;
+          case TRAP_IN:
+            handle_TRAP_IN();
+            break;
+          case TRAP_PUTSP:
+            handle_TRAP_PUTSP();
+            break;
+          case TRAP_HALT:
+            handle_TRAP_HALT();
+            break;
+        }
+        /* handle_TRAP(instruction, regs); */
         break;
       case OP_RES:
         /* Unused */
@@ -193,7 +229,7 @@ uint16_t sign_extend(uint16_t x, int bit_count) {
    * passed value x
    * */
 
-  if ((x >> (bit_count - 1)) & 1) {
+  if ((x >> (bit_count - 1)) & 0x1) {
     /* The condition is triggered if the last bit == 1, indicating a negative
      * number */
     x |= (0xFFFF << bit_count);
@@ -224,7 +260,7 @@ void handle_ADD(uint16_t instruction, uint16_t regs[]) {
   /* First operand (SR1) */
   uint16_t sr1_index = (instruction >> 6) & 0x7;
   /* Whether we are in immediate mode */
-  uint16_t imm_flag = (instruction >> 5) & 1;
+  uint16_t imm_flag = (instruction >> 5) & 0x1;
 
   if (imm_flag) {
     uint16_t imm5 = sign_extend(instruction & 0x1F, 5);
@@ -243,76 +279,66 @@ void handle_ADD(uint16_t instruction, uint16_t regs[]) {
 
 void handle_AND(uint16_t instruction, uint16_t regs[]) {
   /* Gather instruction and filter with bitmask */
-  uint16_t dr_index = instruction >> 9 & 0x7;
-  uint16_t sr1_index = instruction >> 6 & 0x7;
+  uint16_t dr_index = (instruction >> 9) & 0x7;
+  uint16_t sr1_index = (instruction >> 6) & 0x7;
 
-  if (instruction >> 5 & 1) {
+  if ((instruction >> 5) & 0x1) {
     /* If bit [5] == 1, DR = SR1 AND imm5 */
     uint16_t imm5 = sign_extend(instruction & 0x1F, 5);
-    regs[dr_index] = mem_read(regs[sr1_index]) & imm5;
+    regs[dr_index] = regs[sr1_index] & imm5;
 
   } else {
     /* If bit [5] == 0, DR = SR1 AND SR2 */
     uint16_t sr2_index = instruction & 0x7;
-    regs[dr_index] = mem_read(regs[sr1_index]) & mem_read(regs[sr2_index]);
+    regs[dr_index] = regs[sr1_index] & regs[sr2_index];
   }
 
   update_flags(dr_index);
 }
 
 void handle_NOT(uint16_t instruction, uint16_t regs[]) {
-  uint16_t dr_index = instruction >> 9 & 0x7;
-  uint16_t sr_index = instruction >> 6 & 0x7;
-  regs[dr_index] = ~mem_read(regs[sr_index]);
+  uint16_t dr_index = (instruction >> 9) & 0x7;
+  uint16_t sr_index = (instruction >> 6) & 0x7;
+  regs[dr_index] = ~regs[sr_index];
   update_flags(dr_index);
 }
 
 void handle_BR(uint16_t instruction, uint16_t regs[]) {
-  uint16_t local_flag = 0;
-  uint16_t n_flag = instruction >> 11 & 1;
-  uint16_t z_flag = instruction >> 10 & 1;
-  uint16_t p_flag = instruction >> 9 & 1;
+  uint16_t offset = sign_extend(instruction & 0x1FF, 9);
+  uint16_t local_flag = 0x0;
+  uint16_t p_flag = (instruction >> 9) & 0x1;
+  uint16_t z_flag = (instruction >> 10) & 0x1;
+  uint16_t n_flag = (instruction >> 11) & 0x1;
 
-  /* If no condition is checked, set local_flag to 1 unconditionally */
-  if (!(n_flag || z_flag || p_flag))
-    local_flag = 1;
-
-  /* If N's bit [1] is set to 1, test for N */
-  if (n_flag)
-    if (regs[R_COND] == FL_N)
-      local_flag = 1;
-  
-  /* If Z's bit [10] is set to 1, test for Z */
-  if (z_flag)
-    if (regs[R_COND] == FL_Z)
-      local_flag = 1;
-  
-  /* If P's bit [10] is set to 1, test for P */
-  if (p_flag)
-    if (regs[R_COND] == FL_P)
-      local_flag = 1;
+  if (((instruction >> 9) & 0x7) == 0x000) {
+    local_flag = 0x1;
+  } else if ((n_flag == regs[R_COND])
+      || (z_flag == regs[R_COND])
+      || (p_flag == regs[R_COND])) {
+    local_flag = 0x1;
+  }
   
   if (local_flag)
-    regs[R_PC] += sign_extend(instruction & 0x1FF, 9);
+    regs[R_PC] += offset;
 }
 
 void handle_JMP(uint16_t instruction, uint16_t regs[]) {
-  uint16_t baseR_index = instruction >> 6 & 0x7;
+  uint16_t baseR_index = (instruction >> 6) & 0x7;
   regs[R_PC] = regs[baseR_index];
 }
 
 void handle_JSR(uint16_t instruction, uint16_t regs[]) {
   regs[R_R7] = regs[R_PC];
-  if (instruction >> 11 & 1) {
+  if ((instruction >> 11) & 0x1) {
     regs[R_PC] += sign_extend(instruction & 0x7FF, 11);
   } else {
-    uint16_t baseR_index = instruction >> 6 & 0x7;
+    uint16_t baseR_index = (instruction >> 6) & 0x7;
     regs[R_PC] = regs[baseR_index];
   }
 }
 
 void handle_LD(uint16_t instruction, uint16_t regs[]) {
-  uint16_t dr_index = instruction >> 9 & 0x1FF;
+  uint16_t dr_index = (instruction >> 9) & 0x7;
   uint16_t sign_extended_offset = sign_extend(instruction & 0x1FF, 9);
   uint16_t search_address = regs[R_PC] + sign_extended_offset;
   regs[dr_index] = mem_read(search_address);
@@ -322,7 +348,7 @@ void handle_LD(uint16_t instruction, uint16_t regs[]) {
 void handle_LDI(uint16_t instruction, uint16_t regs[]) {
   /* Get the DR (bits [9:11]) by moving starting bit to bit 9, and then use
    * bitmask to filter out exceeding bits */
-  uint16_t dr_index = instruction >> 9 & 0x7;
+  uint16_t dr_index = (instruction >> 9) & 0x7;
   /* Bitmask to only keep bits [0:8] (the contents of PCoffset9) */
   uint16_t relevant_instr = instruction & 0x1FF;
   /* Gather first memory address from PCoffset9 by sign-extending it*/
@@ -345,8 +371,8 @@ void handle_LDI(uint16_t instruction, uint16_t regs[]) {
 }
 
 void handle_LDR(uint16_t instruction, uint16_t regs[]) {
-  uint16_t dr_index = instruction >> 9 & 0x7;
-  uint16_t baseR_index = instruction >> 6 & 0x7;
+  uint16_t dr_index = (instruction >> 9) & 0x7;
+  uint16_t baseR_index = (instruction >> 6) & 0x7;
   uint16_t offset = sign_extend(instruction & 0x3F, 6);
   uint16_t search_address = regs[baseR_index] + offset;
   regs[dr_index] = mem_read(search_address);
@@ -354,31 +380,32 @@ void handle_LDR(uint16_t instruction, uint16_t regs[]) {
 }
 
 void handle_LEA(uint16_t instruction, uint16_t regs[]) {
-  uint16_t dr_index = instruction >> 9 & 0x7;
+  uint16_t dr_index = (instruction >> 9) & 0x7;
   uint16_t offset = sign_extend(instruction & 0x1FF, 9);
   regs[dr_index] = regs[R_PC] + offset;
   update_flags(dr_index);
 }
 
 void handle_ST(uint16_t instruction, uint16_t regs[]) {
-  uint16_t offset = sign_extend(instruction >> 9 & 0x1FF, 9);
-  uint16_t sr_index = instruction >> 9 & 0x7;
-  uint16_t target_address = mem_read(regs[R_PC] + offset);
-  /* TODO: stuck here */
+  uint16_t sr_index = (instruction >> 9) & 0x7;
+  uint16_t offset = sign_extend(instruction & 0x1FF, 9);
+  uint16_t target_address = regs[R_PC] + offset;
+  mem_write(target_address, regs[sr_index]);
 }
 
 void handle_STI(uint16_t instruction, uint16_t regs[]) {
-  uint16_t offset = sign_extend(instruction >> 9 & 0x1FF, 9);
-  uint16_t sr_index = instruction >> 9 & 0x7;
-  uint16_t target_address = mem_read(mem_read(regs[R_PC] + offset));
-  /* TODO: stuck here */
+  uint16_t sr_index = (instruction >> 9) & 0x7;
+  uint16_t offset = sign_extend((instruction >> 9) & 0x1FF, 9);
+  uint16_t target_address = mem_read(regs[R_PC] + offset);
+  mem_write(target_address, regs[sr_index]);
 }
 
 void handle_STR(uint16_t instruction, uint16_t regs[]) {
-  uint16_t baseR_index = instruction >> 6 & 0x7;
+  uint16_t sr_index = (instruction >> 9) & 0x7;
+  uint16_t baseR_index = (instruction >> 6) & 0x7;
   uint16_t offset = sign_extend(instruction & 0x3F, 6); 
   uint16_t target_address = regs[baseR_index] + offset;
-  /* TODO: stuck here */
+  mem_write(target_address, regs[sr_index]);
 }
 
 void handle_TRAP(uint16_t instruction, uint16_t regs[]) {
@@ -388,6 +415,45 @@ void handle_TRAP(uint16_t instruction, uint16_t regs[]) {
 }
 
 void handle_BAD_OPCODE(uint16_t instruction, uint16_t regs[]) {
-  printf("0x000D Bad OP_CODE. Exiting");
-  exit(1);
+  abort();
+}
+
+void handle_TRAP_GETC() {
+  uint16_t c = (uint16_t) getchar();
+  regs[R_R0] = c & 0xFF; /* Clear the last 8 bits */
+}
+
+void handle_TRAP_OUT() {
+  uint16_t c = regs[R_R0] & 0xFF;
+  putc((char) c, stdout);
+}
+
+void handle_TRAP_PUTS() {
+  /* In memory, treat the characters as uint16_t values */
+  
+  /* The string start at memory_start + R0 */
+  uint16_t *c = memory + regs[R_R0];
+
+  /* The string terminates with the occurence of 0x0000 in a memory location
+   * per the specification, so we can use that to knw when to stop the loop */
+  while(*c) {
+    /* Cast the char values to char type (8bit) in order to use the putc()
+     * function, as chars in C are 8bits, but here we stored them as 16bits */
+    putc((char) *c, stdout);
+    c++;
+  }
+
+  fflush(stdout);
+}
+
+void handle_TRAP_IN() {
+  /* TODO */
+}
+
+void handle_TRAP_PUTSP() {
+  /* TODO */
+}
+
+void handle_TRAP_HALT() {
+  /* TODO */
 }
